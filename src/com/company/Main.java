27f192ef52488bbeab7faf62d232bfe9dc330d36
Adam <adprzytula@gmail.com>
2020-08-28 23:37:19 +0200

    Next two methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,15 @@
 package com.company;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.net.SocketOption;
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
         TransitCalculator transitCalculator1 = new TransitCalculator(19,20);
-        TransitCalculator transitCalculator2 = new TransitCalculator(22,50);
-        TransitCalculator transitCalculator3 = new TransitCalculator(6,14);
+        TransitCalculator transitCalculator2 = new TransitCalculator(26,54);
+        TransitCalculator transitCalculator3 = new TransitCalculator(5,12);
 
         System.out.println(transitCalculator1.unlimited7Price());
-        System.out.println(transitCalculator2.unlimited7Price()+ "\n"+ transitCalculator3.unlimited7Price());
+
+
+        System.out.println(transitCalculator2.getBestFare());
+        System.out.println(transitCalculator3.getBestFare());
     }
 }
--- src/com/company/TransitCalculator.java
+++ src/com/company/TransitCalculator.java
@@ -1,10 +1,11 @@
 package com.company;
 
+
 public class TransitCalculator {
     int numberOfDays;
     int expectedNumberOfRides;
-    String ticketName[]={"Pay-per-ride","7-day Unlimited Rides", "30-day Unlimited Rides"};
-    double ticketCost[]={2.75,33,127};
+    String ticketName[] = {"Pay-per-ride","7-day Unlimited Rides", "30-day Unlimited Rides"};
+    double ticketCost[] = {2.75,33,127};
 
 
 
@@ -22,9 +23,32 @@ public class TransitCalculator {
             totalCost=numberOfDays/7*ticketCost[1];
         }
         */
-        totalCost=Math.ceil((double)numberOfDays/7)*ticketCost[1];
+        totalCost = Math.ceil((double)numberOfDays/7)*ticketCost[1];
         return totalCost/expectedNumberOfRides;
     }
 
+    public double[] getRidePrices(){
+        double unlimited7PerRide, unlimited30PerRide;
 
+        unlimited30PerRide = Math.ceil((double)numberOfDays/30)*ticketCost[2]/expectedNumberOfRides;
+        unlimited7PerRide = unlimited7Price();
+
+        double perRideEachOption[] = {ticketCost[0],unlimited7PerRide,unlimited30PerRide};
+        return perRideEachOption;
+    }
+
+    String getBestFare(){
+        double ridePrices[] = getRidePrices();
+        double bestPrice = ridePrices[0];
+        int currentIndex = 0;
+        int lowestFareIndex = 0;
+        for (double ridePrice : ridePrices) {
+            if(bestPrice > ridePrice){
+                bestPrice = ridePrice;
+                lowestFareIndex = currentIndex;
+            }
+            currentIndex++;
+        }
+        return "You should get the "+ticketName[lowestFareIndex]+" option at $"+Math.round(ridePrices[lowestFareIndex]*100.0)/100.0+" per ride.";
+    }
 }

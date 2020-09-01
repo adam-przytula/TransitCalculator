8f10caca1acbc4d4bc877cc2a0b691e1198f360f
Adam <adprzytula@gmail.com>
2020-09-01 23:24:36 +0200

    deleted whitespaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        TransitCalculator transitCalculator1 = new TransitCalculator(19,20, 53, true);
-        TransitCalculator transitCalculator2 = new TransitCalculator(26,54, 68, false);
-        TransitCalculator transitCalculator3 = new TransitCalculator(5,12, 85, true);
-        TransitCalculator transitCalculator4 = new TransitCalculator(7,62, 33, false);
-
+        TransitCalculator transitCalculator1 = new TransitCalculator(19, 20, 53, true);
+        TransitCalculator transitCalculator2 = new TransitCalculator(26, 54, 68, false);
+        TransitCalculator transitCalculator3 = new TransitCalculator(5, 12, 85, true);
+        TransitCalculator transitCalculator4 = new TransitCalculator(7, 62, 33, false);
 
 
         System.out.println(transitCalculator1.getBestFare());
--- src/com/company/TransitCalculator.java
+++ src/com/company/TransitCalculator.java
@@ -18,14 +18,14 @@ public class TransitCalculator {
         this.disabilities = disabilities;
     }
 
-    public double[] getRidePrices(){
+    public double[] getRidePrices() {
         double unlimited7PerRide, unlimited30PerRide;
-        double [] perRideEachOption;
-        if(age < 65 && disabilities == false) {
+        double[] perRideEachOption;
+        if (age < 65 && disabilities == false) {
             unlimited30PerRide = Math.ceil((double) numberOfDays / 30) * ticketCost[2] / expectedNumberOfRides;
             unlimited7PerRide = Math.ceil((double) numberOfDays / 7) * ticketCost[1] / expectedNumberOfRides;
             perRideEachOption = new double[]{ticketCost[0], unlimited7PerRide, unlimited30PerRide};
-        } else{
+        } else {
             unlimited30PerRide = Math.ceil((double) numberOfDays / 30) * ticketReducedCost[2] / expectedNumberOfRides;
             unlimited7PerRide = Math.ceil((double) numberOfDays / 7) * ticketReducedCost[1] / expectedNumberOfRides;
             perRideEachOption = new double[]{ticketReducedCost[0], unlimited7PerRide, unlimited30PerRide};
@@ -33,18 +33,18 @@ public class TransitCalculator {
         return perRideEachOption;
     }
 
-    String getBestFare(){
+    String getBestFare() {
         double ridePrices[] = getRidePrices();
         double bestPrice = ridePrices[0];
         int currentIndex = 0;
         int lowestFareIndex = 0;
         for (double ridePrice : ridePrices) {
-            if(bestPrice > ridePrice){
+            if (bestPrice > ridePrice) {
                 bestPrice = ridePrice;
                 lowestFareIndex = currentIndex;
             }
             currentIndex++;
         }
-        return "You should get the "+ticketName[lowestFareIndex]+" option at $"+Math.round(ridePrices[lowestFareIndex]*100.0)/100.0+" per ride.";
+        return "You should get the " + ticketName[lowestFareIndex] + " option at $" + Math.round(ridePrices[lowestFareIndex] * 100.0) / 100.0 + " per ride.";
     }
 }
